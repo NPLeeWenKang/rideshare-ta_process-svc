beefb6e4af4656a91154b8fbcb0c3e087350f9a9
NPLeeWenKang <s10203100@connect.np.edu.sg>
2022-12-16 02:05:55 +0800

    removed unneeded types

--- passanger.go
+++
@@ -1,9 +0,0 @@
-package main
-
-type Passanger struct {
-	Passanger_Id int    `json:"passanger_id"`
-	First_Name   string `json:"first_name"`
-	Last_Name    string `json:"last_name"`
-	Mobile_No    string `json:"mobile_no"`
-	Email        string `json:"email"`
-}
